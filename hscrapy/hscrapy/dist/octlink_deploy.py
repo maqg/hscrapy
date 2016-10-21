4d2afa96666926e08f46598fe8fc83c1dbae48e0
maqg <maquangag@163.com>
2016-10-21 17:03:32 +0800

    Add web site pages

--- hscrapy/hscrapy/dist/octlink_deploy.py
+++ hscrapy/hscrapy/dist/octlink_deploy.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import json
+
+from hscrapy.utils.commonUtil import fileToObj
 
 SOURCE_FONFIG_FILE = "octlink_sources.json"
 
 def deploy_octlink():
-	pass
+	obj = fileToObj(SOURCE_FONFIG_FILE)
+	print(json.dumps(obj['webSites'], indent=4, ensure_ascii=False))
+	print(json.dumps(obj["webSites"], ensure_ascii=False).replace("\"", "\\\""))
 
 if __name__ == "__main__":
 
